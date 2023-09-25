7106a47bb54e9d02de82e20dbc618f4a93b77f03
mamazoirovm <mirzarajimk@gmail.com>
2023-09-25 21:50:47 +0500

    qoshimcha stillar yakunlandi, loyiha yakunlandi.

---
+++ app.js
@@ -0,0 +1,17 @@
+const dark = document.querySelector(".dark");
+const Body = document.querySelector("body");
+let isDarkMode = false;
+
+dark.addEventListener("click", () => {
+    if (isDarkMode) {
+        Body.style.background = "black";
+        dark.style.marginLeft = "70px"
+        dark.textContent = "OFF"
+        isDarkMode = false;
+    } else {
+        Body.style.background = "#1B1B32"; 
+        dark.style.marginLeft = "0px"
+        dark.textContent = "ON"
+        isDarkMode = true;
+    }
+});
